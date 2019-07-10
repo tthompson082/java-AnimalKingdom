5acb358d18e790ebf5d64db540077444d91021ed
tthompson082 <tthompson082@gmail.com>
2019-07-10 15:59:45 -0400

    add Fish.java; added all animals to Main.java

---
+++ src/animalkingdom/Fish.java
@@ -0,0 +1,33 @@
+package animalkingdom;
+
+public class Fish extends AbstractAnimals
+{
+    public Fish(String name, int yearDiscovered)
+    {
+        super(name, yearDiscovered);
+    }
+
+    @Override
+    public String getMove()
+    {
+        return "Swim";
+    }
+
+    @Override
+    public String getBreath()
+    {
+        return "Gills";
+    }
+
+    @Override
+    public String getReproduce()
+    {
+        return "Eggs";
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Fish {" + " name = " + getName() + ", year discovered = " + getYearDiscovered() + " }";
+    }
+}
--- src/animalkingdom/Main.java
+++ src/animalkingdom/Main.java
@@ -8,7 +8,20 @@ public class Main
     {
         Mammals panda = new Mammals("Panda", 1869);
         Mammals zebra = new Mammals("Zebra", 1778);
-        System.out.println(panda.getId());
-        System.out.println(zebra.getId());
+        Mammals koala = new Mammals("Koala", 1816);
+        Mammals sloth = new Mammals("Sloth", 1804);
+        Mammals armadillo = new Mammals("Armadillo", 1758);
+        Mammals raccoon = new Mammals("Raccoon", 1758);
+        Mammals bigfoot = new Mammals("Bigfoot", 2021);
+
+        Birds pigeon = new Birds("Pigeon", 1837);
+        Birds peacock = new Birds("Peacock", 1821);
+        Birds toucan = new Birds("Toucan", 1758);
+        Birds parrot = new Birds("Parrot", 1824);
+        Birds swan = new Birds("Swan", 1758);
+
+        Fish salmon = new Fish("Salmon", 1758);
+        Fish catfish = new Fish("Catfish", 1817);
+        Fish perch = new Fish("Perch", 1758);
     }
 }
